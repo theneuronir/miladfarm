2da43b6c570756c827abe1872ca5319e12223846
Masoud <masoud@theneuron.ir>
2022-11-26 22:57:38 +0330

    fd0e471

---
+++ _nuxt/static/1669490849/products/mojito-mint/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmiladfarm\u002F_nuxt\u002Fstatic\u002F1669490849",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproducts\u002Fmojito-mint",config:{_app:{basePath:"\u002Fmiladfarm\u002F",assetsPath:"\u002Fmiladfarm\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"abc0ae7b"}}}}(null));
